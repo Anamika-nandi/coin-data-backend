79cd9712d7767ccb821e8b12a891686576db5db2
Anamika-Nandi <nandianamika143@gmail.com>
2022-10-19 22:14:39 +0530

    minor changes

--- services/CoinDataService.js
+++ services/CoinDataService.js
@@ -5,5 +5,17 @@ exports.getAllData = async () => {
   };
 
   exports.createCoinData = async (data) => {
-    return await CoinDataModel.create(data);
+    const start = new Date();
+    start.setHours(0,0,0,0);
+
+    const end = new Date();
+    end.setHours(23,59,59,999);
+    const dataToday = await CoinDataModel.find({created_on: {$gte: start, $lt: end}})
+    if(dataToday.length<50)
+      return await CoinDataModel.create(data);
+    else{
+      const error = new Error("Limit to add 50 coins reached")
+      error.code = 401
+      throw error
+    }
   };
